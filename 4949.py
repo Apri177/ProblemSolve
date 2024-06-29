f3a884eba23d919ff594c9f99223a766e2852d36
april <architt7274@gmail.com>
2024-06-29 23:30:02 +0900

    n

---
+++ 4949.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+while(1):
+    small = 0
+    big = 0
+    world = input()
+
+    print(world == '.')
+
+    if world == '.':
+        break
+
+    for t in world:
+        if t == '(':
+            small += 1
+        elif t == ')':
+            small -= 1
+        elif t == '[':
+            big += 1
+        elif t == ']':
+            big -= 1
+        if small < 0 or big < 0:
+            print('no')
+            break
+    if small == 0 and big == 0:
+        print('yes')
